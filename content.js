8dc2292d846dc51ae233cc910cc49453a454643f
mohamedkante <114979734+mohamedkante@users.noreply.github.com>
2023-01-17 10:22:25 +0100

    Mise à jour commit

--- content.js
+++ content.js
@@ -1,1 +1,26 @@
-alert("C'EST L'HEURE DE MANGER")
+
+ function AfficheNotification (hour, minute) {
+    let now = new Date();
+    let notificationTime = new Date();
+    notificationTime.setHours(hour);
+    notificationTime.setMinutes(minute);
+    notificationTime.setSeconds(0);
+    
+
+     if (notificationTime < now){
+         notificationTime.setdate(notificationTime.getDate() +1);
+      }
+     let delay = notificationTime - now;
+     if (Notification.permission !=="granted") {
+         Notification.requestPermission();
+
+    }
+     setTimeout(function(){
+     new Notification("Notification", { body: "C'EST L'HEURE DE MANGER!", icon : 'images-icon128.png'
+   
+ });
+ }, delay);
+
+}
+ 
+AfficheNotification(10,15);
